5b89164657dce64b69ca2981b0d40e89fc2741e0
Saurabhsharma798 <k.saurabhsharma798@gmail.com>
2025-08-29 15:41:35 +0530

    middlewares practice

---
+++ middleware.js
@@ -0,0 +1,18 @@
+// const express= require("express")
+// const app=express();
+// app.use(express.json())
+// function isOldEnough(req,res,next){
+//     age=req.body.age
+//     if (age>14){
+//         next();
+//     }else{
+//         res.status(401).json({msg:"you are underage"})
+//     }
+// }
+
+
+// // app.get("/ride",isOldEnough,(req,res)=>{
+//     res.json({msg:"you can go with this ride"})
+// })
+
+// app.listen(3000);
